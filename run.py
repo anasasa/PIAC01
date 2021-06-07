9a008874f29ba1bd964a6eb196084ee3e56c3037
drut <drut@paac.localdomain>
2021-06-07 21:32:28 +0200

    redesing

--- run.py
+++ run.py
@@ -2,25 +2,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-  return render_template("base.html")
-
-@app.route('/index')
 def index():
   return render_template("index.html")
 
-@app.route('/about')
-def about():
-  return render_template("aboutme.html")
-  
-@app.route('/contact')
-def contact():
-  return render_template("contact.html")
-
-@app.route('/gallery')
-def gallery():
-  return render_template("gallery.html")
-
 if __name__ == '__main__':
-#  app.run(debug=True)
+#  app.run(debug="True",host="0.0.0.0",port=5000)
   app.run(host='0.0.0.0')
